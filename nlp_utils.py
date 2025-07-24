490d5307c80eb983321097ffc3b1c41c75568129
Mauli Patel <124795820+itsmemauliii@users.noreply.github.com>
2025-07-24 21:32:00 +0530

    Update nlp_utils.py

--- nlp_utils.py
+++ nlp_utils.py
@@ -6,12 +6,3 @@ try:
 except OSError:
     download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
-
-def parse_task_input(text):
-    doc = nlp(text)
-    tasks = []
-    for sent in doc.sents:
-        task = sent.text.strip()
-        if task:
-            tasks.append(task)
-    return tasks
